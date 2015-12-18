ae69b9a345401edafe4b8509897804662e47192d
Mariano Molina <xsin.crueldadx@gmail.com>
2015-12-17 23:05:39 -0300

    Implementación de Vegas Slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,63 @@
-console.log('\'Allo \'Allo!');
+/* jshint smarttabs:true */
+/* global $, aload, google */
+
+// Dinamic Height
+function calculateHeight(element) {
+	'use strict';
+	var windowHeight = $(window).height();
+//	var finalHeight = windowHeight - $('header').height();
+	$(element).height(windowHeight);
+}
+
+// Load event
+$(window).load(function() {
+	'use strict';
+	aload();
+	calculateHeight('.full-height');
+});
+
+// Document ready
+$(document).ready(function() {
+	'use strict';
+	// resize
+	$(window).resize(function() { calculateHeight('.full-height'); });
+
+	// anchor navigation
+	$('a[href*=#]:not([href=#], .btn_plus)').click(function() {
+		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+			var target = $(this.hash);
+			//console.log('Target: ' + target);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				if(this.hash === '#nosotros'){
+					openMasInfo(target.find('.btn_plus').attr('href'));
+				} else {
+					$('html, body').animate({ scrollTop: target.offset().top }, 1000);
+				}
+				return false;
+			}
+		}
+	});
+
+
+	// vegasSlider
+	$('#quienesSomosView').vegas({
+		slides: [
+			{ src: 'images/img-slider-main-1.jpg' },
+			{ src: 'images/img-slider-main-2.jpg' }
+		]
+	});
+
+	// menu
+	$('.lines-button').click(function(event) {
+		event.preventDefault();
+		$(this).toggleClass('close');
+		$('.menu').toggleClass('open animated fadeIn');
+	});
+	// menu item click
+	$('.menu li a').click(function() {
+		$('.lines-button').removeClass('open');
+		$('.menu').toggleClass('open animated fadeIn');
+	});
+
+});
