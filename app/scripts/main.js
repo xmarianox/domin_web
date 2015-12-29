bc3042d22b26f614ce8899ae2fc7f16b6decd4b8
Mariano Molina <xsin.crueldadx@gmail.com>
2015-12-29 10:58:15 -0300

    implementación vista detalle quines somos

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,17 +11,13 @@ $(window).load(function() {
 $(document).ready(function() {
 	'use strict';
 	// anchor navigation
-	$('a[href*=#]:not([href=#], .btn_plus)').click(function() {
+	$('a[href*=#]:not([href=#])').click(function() {
 		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
 			var target = $(this.hash);
 			//console.log('Target: ' + target);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 			if (target.length) {
-				if(this.hash === '#nosotros'){
-					openMasInfo(target.find('.btn_plus').attr('href'));
-				} else {
-					$('html, body').animate({ scrollTop: target.offset().top }, 1000);
-				}
+				$('html, body').animate({ scrollTop: target.offset().top }, 1000);
 				return false;
 			}
 		}
