fc0bf855958340082e60888e6c3882ac73b19a64
Mariano Molina <xsin.crueldadx@gmail.com>
2016-01-02 22:06:38 -0300

    Implementación de productos y servicios en maritimoView

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -52,11 +52,45 @@ $(document).ready(function() {
 		$('html, body').animate({ scrollTop: $(window).height() }, 1000);
 	});
 
-	// items slider
-	$('.item-carousel ul').slick({
-		arrows: true,
-		infinite: false,
-		mobileFirst: true
+	$('#terrestreView a').click(function(event) {
+		event.preventDefault();
+		var target = $(this).attr('data-target');
+		$(target).toggleClass('open animated slideInUp');
+
+		if (target === "#terrestreDetailProduct") {
+			$('.terrestreSlider').slick({
+				arrows: true,
+				infinite: false,
+				mobileFirst: true
+			});
+		}
+	});
+
+	$('#maritimoView a').click(function(event) {
+		event.preventDefault();
+		var target = $(this).attr('data-target');
+		$(target).toggleClass('open animated slideInUp');
+
+		if (target === "#maritimoDetailServicio") {
+			$('.maritimoSlider').slick({
+				arrows: true,
+				infinite: false,
+				mobileFirst: true
+			});
+		} else {
+			$('#maritimoDetailProducto .row').slick({
+				arrows: true,
+				infinite: false,
+				mobileFirst: true,
+				slidesToShow: 6
+			});
+
+			$('.maritimoProductoSlider').slick({
+				arrows: true,
+				infinite: false,
+				mobileFirst: true
+			});
+		}
 	});
 
 });
